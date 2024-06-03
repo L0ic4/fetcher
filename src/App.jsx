de45ba0d7c1282a1402be6f935b842f7609b04df
L01C4 <boy-unfind@proton.me>
2024-06-04 00:31:03 +0300

    feat: add status code in history

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,10 @@ function App() {
       };
       const res = await axios(config);
       setResponse(res.data);
-      setHistory([...history, { method, url }]);
+      setHistory([...history, { method, url, status: res.status }]);
     } catch (error) {
       setResponse(error.message);
-      setHistory([...history, { method, url }]);
+      setHistory([...history, { method, url, status: error.message }]);
     }
   };
   const resetFields = () => {
@@ -122,12 +122,12 @@ function App() {
         </div>
         <div>
           <h2 className="text-xl font-bold my-8">Request History</h2>
-          <div className="border rounded p-4">
+          <div className="border rounded p-4 overflow-x-auto overflow-y-auto max-h-80">
             <ul>
               {history.map((req, index) => (
                 <li key={index} className="mb-2 p-2 rounded border">
                   <span className="text-sm w-full">
-                    {req.method} - {req.url}
+                    {req.method} - {req.url} - {req.status}
                   </span>
                 </li>
               ))}
