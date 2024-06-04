cb97a073da6a440e2c4ab4cec483f0867b83c6be
L01C4 <boy-unfind@proton.me>
2024-06-04 20:21:14 +0300

    feat: refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { FaRocket } from "react-icons/fa";
-import { generateTypeDeclaration } from "../utils/utils";
 import { Footer } from "./components/Footer";
 import { TypeDeclaration } from "./components/TypeDeclaration";
 import { Response } from "./components/Response";
 import { History } from "./components/History";
 import { TextArea } from "./components/TextArea";
 import { UrlInput } from "./components/UrlInput";
-
-const initialHeaders = '{"Authorization": "Bearer YOUR_TOKEN_HERE"}';
-const initialBody = '{"test": "test"}';
-const initialUrl = "https://exemple.com";
-const initialMethod = "GET";
+import {
+  updateTypeDeclaration,
+  resetFields,
+  initialHeaders,
+  initialBody,
+  initialUrl,
+  initialMethod,
+} from "../utils/utils";
 
 function App() {
   const [headers, setHeaders] = useState(initialHeaders);
@@ -45,23 +47,6 @@ function App() {
     }
   };
 
-  const updateTypeDeclaration = (data) => {
-    if (Array.isArray(data) && data.length > 0) {
-      setTypeDeclaration(generateTypeDeclaration(data[0]));
-    } else if (typeof data === "object" && data !== null) {
-      setTypeDeclaration(generateTypeDeclaration(data));
-    }
-  };
-
-  const resetFields = () => {
-    setHeaders(initialHeaders);
-    setBody(initialBody);
-    setResponse(null);
-    setUrl(initialUrl);
-    setMethod(initialMethod);
-    setTypeDeclaration("");
-  };
-
   return (
     <>
       <div className="flex justify-center items-center m-12">
@@ -73,7 +58,7 @@ function App() {
       <div className="bg-card text-card-foreground container m-auto p-12 rounded border">
         <div className="flex flex-col gap-10">
           <h2 className="text-xl font-bold">Request</h2>
-          <div className="border p-4 rounded flex flex-row flex-nowrap gap-2">
+          <div className="border p-4 rounded flex flex-row flex-wrap gap-2">
             <select
               className="bg-background rounded border px-3 py-2 outline-none ring-indigo-300 transition duration-100 focus:ring"
               value={method}
@@ -103,12 +88,21 @@ function App() {
             </button>
             <button
               className="bg-secondary rounded text-foreground px-8"
-              onClick={resetFields}
+              onClick={() =>
+                resetFields({
+                  setHeaders,
+                  setBody,
+                  setResponse,
+                  setUrl,
+                  setMethod,
+                  setTypeDeclaration,
+                })
+              }
             >
               Reset
             </button>
           </div>
-          <div className="rounded border p-3 flex flex-row flex-nowrap gap-6">
+          <div className="rounded border p-3 flex flex-row flex-wrap gap-6">
             <TextArea
               className="bg-background flex-1 rounded border px-3 py-2 outline-none ring-indigo-300 transition duration-100 focus:ring"
               placeholder="Headers (JSON)"
