05cdea1c82c9d7cc12a7f318ee74be4c9a48ea0c
L01C4 <boy-unfind@proton.me>
2024-06-04 20:47:57 +0300

    Revert "feat: refactoring"
    This reverts commit cb97a073da6a440e2c4ab4cec483f0867b83c6be.

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,9 +1,3 @@
-export const initialHeaders = '{"Authorization": "Bearer YOUR_TOKEN_HERE"}';
-export const initialBody = '{"test": "test"}';
-export const initialUrl = "https://exemple.com";
-export const initialMethod = "GET";
-
-
 export const generateTypeDeclaration = obj => {
     let interfaceName = 'GeneratedInterface'; // Nom par défaut de l'interface
     let typeDeclaration = `export interface ${interfaceName} {\n`;
@@ -17,20 +11,3 @@ export const generateTypeDeclaration = obj => {
     return typeDeclaration;
 };
 
-
-export const updateTypeDeclaration = ({data, setTypeDeclaration}) => {
-    if (Array.isArray(data) && data.length > 0) {
-      setTypeDeclaration(generateTypeDeclaration(data[0]));
-    } else if (typeof data === "object" && data !== null) {
-      setTypeDeclaration(generateTypeDeclaration(data));
-    }
-  };
-
-export  const resetFields = ({setHeaders, setBody, setResponse, setUrl, setMethod, setTypeDeclaration}) => {
-    setHeaders(initialHeaders);
-    setBody(initialBody);
-    setResponse(null);
-    setUrl(initialUrl);
-    setMethod(initialMethod);
-    setTypeDeclaration("");
-  };
